0c69ecb530fde23384a63e1a1e73981da212e1df
evdavs <evan.davies@outlook.com>
2024-03-24 19:25:46 +0100

    new myNewEnv version

--- test/learn/myNewEnv.h
+++ test/learn/myNewEnv.h
@@ -1,34 +1,89 @@
-/**
- * Copyright or © or Copr. IETR/INSA - Rennes (2019 - 2020) :
- *
- * Karol Desnos <kdesnos@insa-rennes.fr> (2019 - 2020)
- *
- * GEGELATI is an open-source reinforcement learning framework for training
- * artificial intelligence based on Tangled Program Graphs (TPGs).
- *
- * This software is governed by the CeCILL-C license under French law and
- * abiding by the rules of distribution of free software. You can use,
- * modify and/ or redistribute the software under the terms of the CeCILL-C
- * license as circulated by CEA, CNRS and INRIA at the following URL
- * "http://www.cecill.info".
- *
- * As a counterpart to the access to the source code and rights to copy,
- * modify and redistribute granted by the license, users are provided only
- * with a limited warranty and the software's author, the holder of the
- * economic rights, and the successive licensors have only limited
- * liability.
- *
- * In this respect, the user's attention is drawn to the risks associated
- * with loading, using, modifying and/or developing or reproducing the
- * software by the user in light of its specific status of free software,
- * that may mean that it is complicated to manipulate, and that also
- * therefore means that it is reserved for developers and experienced
- * professionals having in-depth computer knowledge. Users are therefore
- * encouraged to load and test the software's suitability as regards their
- * requirements in conditions enabling the security of their systems and/or
- * data to be ensured and, more generally, to use and operate it in the
- * same conditions as regards security.
- *
- * The fact that you are presently reading this means that you have had
- * knowledge of the CeCILL-C license and that you accept its terms.
- */
+#ifndef FAKE_FEDERATED_LEARNING_ENVIRONMENT_H
+#define FAKE_FEDERATED_LEARNING_ENVIRONMENT_H
+
+#include "learn/learningEnvironment.h"
+#include "data/arrayWrapper.h"
+#include <array>
+
+namespace Learn {
+
+    /**
+     * \brief FakeLearningEnvironment class for testing purposes.
+     *
+     * This class simulates a fake learning environment for testing and debugging.
+     * It returns fake data in the form of an ArrayWrapper with pre-known numbers.
+     */
+    class myNewEnv : public LearningEnvironment {
+      private:
+        /// Vector of references to data sources
+        std::vector<std::reference_wrapper<const Data::DataHandler>> dataSources;
+
+      public:
+        /**
+         * \brief Constructor for myNewEnv.
+         *
+         * \param[in] nbActions Number of actions available for interacting with this Environement.
+         * \param[in] fakeData Pre-known data source.
+         */
+        myNewEnv(uint64_t nbActions, const std::vector<double>& fakeData)
+            : LearningEnvironment(nbActions) {
+            // Create ArrayWrapper instances for pre-known data sources
+            auto* array = new Data::ArrayWrapper<double>(fakeData.size());
+            array->setPointer(new std::vector<double>(fakeData));
+            dataSources.push_back(*array);
+        }
+
+        /**
+         * \brief Implementation of the reset method for myNewEnv.
+         *
+         * \param[in] seed The integer value for controlling the randomness of the myNewEnv.
+         * \param[in] mode LearningMode in which the myNewEnv should be reset.
+         * \param[in] iterationNumber The integer value to indicate the current iteration number.
+         * \param[in] generationNumber The integer value to indicate the current generation number.
+         */
+        void reset(size_t seed = 0, LearningMode mode = LearningMode::TRAINING, uint16_t iterationNumber = 0, uint64_t generationNumber = 0) override {
+            // Reset the fake data (not relevant for this fake environment).
+        }
+
+        /**
+         * \brief Implementation of the doAction method for myNewEnv.
+         *
+         * \param[in] actionID The integer number representing the action to execute.
+         * \throw std::runtime_error if the actionID exceeds nbActions - 1.
+         */
+        void doAction(uint64_t actionID) override {
+            // Perform the action (not relevant for this environment).
+        }
+
+        /**
+         * \brief Implementation of the getDataSources method for myNewEnv.
+         *
+         * \return A vector of references to the fake data sources.
+         */
+        std::vector<std::reference_wrapper<const Data::DataHandler>> getDataSources() override {
+            return dataSources;
+        }
+
+        /**
+         * \brief Implementation of the getScore method for myNewEnv.
+         *
+         * \return The current score for the myNewEnv.
+         */
+        double getScore() const override {
+            // Return a fixed score for simplicity.
+            return 0.0;
+        }
+
+        /**
+         * \brief Implementation of the isTerminal method for myNewEnv.
+         *
+         * \return A boolean indicating whether the myNewEnv has reached a terminal state.
+         */
+        bool isTerminal() const override {
+            // Return false for simplicity.
+            return false;
+        }
+    };
+}; // namespace Learn
+
+#endif
